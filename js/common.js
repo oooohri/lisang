df3946ed43eab618284d2d3f70203c896bff4710
oooohri <oooorrrr0117@gmail.com>
2025-01-24 17:29:52 +0900

    css 수정

--- js/common.js
+++ js/common.js
@@ -6,5 +6,8 @@ $(window).on('load', function(){
 	$('.gnb-toggle').on('click', function(){
 		$('body').toggleClass('gnb-open');
 	});
-
+	
+	$(document).ready(function() {
+		var easyzoom = $('.easyzoom').easyZoom(); // easyzoom 클래스를 가진 요소들에 대해 easyZoom 초기화
+	});
 });
